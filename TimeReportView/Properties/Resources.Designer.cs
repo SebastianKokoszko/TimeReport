51528c40933cbc4bb072bd29f385a86330433a8d
Sebastian Kokoszko <seb-k@hotmail.com>
2017-01-05 14:47:59 +0100

    Added data model project. Change project names.  Setup code analysis on build.

--- TimeReportView/Properties/Resources.Designer.cs
+++ TimeReportView/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace TimeReportView.Properties {
+namespace TimeReport.DesktopView.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace TimeReportView.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TimeReportView.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TimeReport.DesktopView.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
